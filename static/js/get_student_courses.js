264827f3585bca50b90d2bddd10e60605ed25c2d
Oleksandr Korienev <alexkorienev@gmail.com>
2018-06-02 08:32:25 +0300

    duplicated menu items insertion

--- static/js/get_student_courses.js
+++ static/js/get_student_courses.js
@@ -1,8 +1,14 @@
 /**
  * Created by Alex Korienev on 5/26/18.
  */
-type="module";
-import insert_menu_items from "insert_menu_items";
+function insert_menu_items(data, status, xhrObject) {
+    let menu_children = $.find("#pure-menu-children")[0];
+    for (i in data){
+        menu_children.innerHTML += '<li class="pure-menu-item"><a href={url} class="pure-menu-link">{name}</a></li>'
+                .replace("{url}", data[i].url).replace("{name}", data[i].name);
+    }
+}
+
 $.get({
     url:"/student/ajax/my-courses/",
     success: insert_menu_items
--- static/js/get_tutor_courses.js
+++ static/js/get_tutor_courses.js
@@ -1,8 +1,14 @@
 /**
  * Created by Alex Korienev on 5/28/18.
  */
-type="module";
-import insert_menu_items  from 'insert_menu_items'
+function insert_menu_items(data, status, xhrObject) {
+    let menu_children = $.find("#pure-menu-children")[0];
+    for (i in data){
+        menu_children.innerHTML += '<li class="pure-menu-item"><a href={url} class="pure-menu-link">{name}</a></li>'
+                .replace("{url}", data[i].url).replace("{name}", data[i].name);
+    }
+}
+
 $.get({
     url:"/tutor/courses_ajax",
     success: insert_menu_items
