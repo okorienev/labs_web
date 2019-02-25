51c4c790dd1909a0c48e4c2fbde930e744e39b1f
Oleksandr Korienev <alexkorienev@gmail.com>
2019-02-25 13:30:48 +0200

    add window size and page loading wait

--- labs_web/test/AbstractTest.py
+++ labs_web/test/AbstractTest.py
@@ -1,5 +1,6 @@
 import unittest
 from selenium.webdriver.chrome.options import Options
+from .Config import TestConfig
 from selenium import webdriver
 
 
@@ -8,6 +9,7 @@ class AbstractTest(unittest.TestCase):
         chrome_opts = Options()
         chrome_opts.add_argument("--headless")
         chrome_opts.add_argument("--no-sandbox")
+        chrome_opts.add_argument("window-size=" + str(TestConfig.SCREEN_WIDTH) + 'x' + str(TestConfig.SCREEN_HEIGHT))
         self.driver = webdriver.Chrome(options=chrome_opts)
 
     def tearDown(self):
--- labs_web/test/Config.py
+++ labs_web/test/Config.py
@@ -2,4 +2,6 @@ class TestConfig:
     SERVER_URL = "localhost"
     SERVER_PORT = 80
     DEFAULT_PASSWORD = 'password'
+    SCREEN_WIDTH = 1366
+    SCREEN_HEIGHT = 768
 
