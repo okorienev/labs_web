37bfe7208219af07e46823a67af8b14d1278ddd3
Annibezh <anytkin11@gmail.com>
2020-06-01 20:35:44 +0300

    Add naming conventions, bump sqlalchemy version

--- labs_web/extensions/extensions.py
+++ labs_web/extensions/extensions.py
@@ -1,14 +1,27 @@
+from alembic.util import immutabledict
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_caching import Cache
 from flask_mail import Mail
 from celery import Celery
+from sqlalchemy import MetaData
+
 from labs_web.config import Config, NonDockerConfig
 from flask_admin import Admin
 from flask_ckeditor import CKEditor
 from pymongo import MongoClient
 
-db = SQLAlchemy()
+
+naming_convention = immutabledict({
+    'uq': '%(table_name)s_%(column_0_N_name)s_key',    # unique constraint
+    'ix': '%(table_name)s_%(column_0_N_name)s_idx',    # index
+    'pk': '%(table_name)s_pkey',                       # primary key
+    'fk': '%(table_name)s_%(column_0_N_name)s_fkey',   # foreign key
+    'ck': '%(table_name)s_%(column_0_N_name)s_check',  # check constraint
+})
+metadata = MetaData(naming_convention=naming_convention)
+
+db = SQLAlchemy(metadata=metadata)
 login_manager = LoginManager()
 cache = Cache()
 mail = Mail()
